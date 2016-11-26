bcbd09074baa3c4cbbdf1ddc5f55d3a9a10ed2e3
Maila Manzur <mailamanzur@mailas-mbp.home>
2016-11-26 15:07:21 -0200

    Initial Commit

---
+++ OlxCodeChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OlxCodeChallenge
+//
+//  Created by Maila  Manzur on 26/11/16.
+//  Copyright © 2016 Maila  Manzur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OlxCodeChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OlxCodeChallenge
+//
+//  Created by Maila  Manzur on 26/11/16.
+//  Copyright © 2016 Maila  Manzur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
